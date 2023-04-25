b16b321025becd58a5a2b10321786978be11fc0b
olegPavlushenkoHelix <oleh.pavlushenko@gmail.com>
2023-04-26 00:00:56 +0300

    add badge, timer. rating

--- scripts/grid-list-view.js
+++ scripts/grid-list-view.js
@@ -5,59 +5,35 @@ class GridListView extends HTMLElement {
         this.gridBtn = this.querySelector(".js-grid-view")
         this.listBtn = this.querySelector(".js-list-view")
         this.productGrid = document.querySelector(".js-product-grid")
+        this.viewButtons = this.querySelectorAll(".js-view-button")
 
-        this.gridBtn.addEventListener(
-            "click",
-            ((event) => {
+        this.viewButtons.forEach((button) => {
+            button.addEventListener("click", (event) => {
                 event.preventDefault()
-                this.gridBtn.classList.add("active")
-                this.listBtn.classList.remove("active")
-                this.switchView("grid")
-            }).bind(this)
-        )
-
-        this.listBtn.addEventListener(
-            "click",
-            ((event) => {
-                event.preventDefault()
-                this.listBtn.classList.add("active")
-                this.gridBtn.classList.remove("active")
-                this.switchView("list")
-            }).bind(this)
-        )
+                this.viewButtons.forEach((btn) => {
+                    btn.classList.toggle("active", btn === button)
+                })
+                const view = button.dataset.view
+                this.switchView(view)
+            })
+        })
 
         this.checkLocalStorage()
     }
 
     switchView(view) {
-        if (view === "grid") {
-            this.productGrid.classList.add("grid")
-            this.productGrid.classList.remove("list")
-        } else if (view === "list") {
-            this.productGrid.classList.add("list")
-            this.productGrid.classList.remove("grid")
-        }
-        // Save the state to local storage
+        const activeClass = view === "grid" ? "grid" : "list"
+        const inactiveClass = view === "grid" ? "list" : "grid"
+        this.productGrid.classList.add(activeClass)
+        this.productGrid.classList.remove(inactiveClass)
         localStorage.setItem("view", view)
     }
 
     checkLocalStorage() {
-        const previousView = localStorage.getItem("view")
-        if (previousView) {
-            this.switchView(previousView)
-            if (previousView === "grid") {
-                this.gridBtn.classList.add("active")
-                this.listBtn.classList.remove("active")
-            } else {
-                this.listBtn.classList.add("active")
-                this.gridBtn.classList.remove("active")
-            }
-        } else {
-            // Default view is grid
-            this.switchView("grid")
-            this.gridBtn.classList.add("active")
-            this.listBtn.classList.remove("active")
-        }
+        const previousView = localStorage.getItem("view") || "grid"
+        this.switchView(previousView)
+        this.gridBtn.classList.toggle("active", previousView === "grid")
+        this.listBtn.classList.toggle("active", previousView === "list")
     }
 }
 
