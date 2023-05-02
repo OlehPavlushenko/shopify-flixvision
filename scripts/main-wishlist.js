262602f8a22a1207b1f1f793044424fc8e229cae
olegPavlushenkoHelix <oleh.pavlushenko@gmail.com>
2023-05-03 00:11:19 +0300

    іstyle page wishlist, add global settings of card-product

--- scripts/main-wishlist.js
+++ scripts/main-wishlist.js
@@ -12,51 +12,55 @@ class PageWishlist extends HTMLElement {
         let namesString = productNames.join("=")
 
         let pageResults = document.querySelector(".js-list-container")
-        let imageSize = pageResults.getAttribute("data-size")
+        let titleResults = document.querySelector(".js-page-wishlist-title")
+        if (namesString) {
+            titleResults.classList.add("hidden")
+            fetch(
+                window.Shopify.routes.root +
+                    "collections/all?section_id=page-wishlist&handle=" +
+                    encodeURIComponent(namesString)
+            )
+                .then((response) => {
+                    if (!response.ok) {
+                        throw new Error("Network response was not ok")
+                    }
+                    return response.text()
+                })
+                .then((text) => {
+                    const html = document.createElement("div")
+                    html.innerHTML = text
+                    const pageBlock = html.querySelector(".js-page-block")
 
-        fetch(
-            window.Shopify.routes.root +
-                "collections/all?section_id=page-wishlist&handle=" +
-                encodeURIComponent(namesString)
-        )
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error("Network response was not ok")
-                }
-                return response.text()
-            })
-            .then((text) => {
-                const html = document.createElement("div")
-                html.innerHTML = text
-                const pageBlock = html.querySelector(".js-page-block")
+                    if (pageBlock && pageBlock.innerHTML.trim().length) {
+                        pageResults.innerHTML = pageBlock.innerHTML
+                        let btnAddWishlistAll =
+                            pageResults.querySelectorAll(".js-btn-wishlist")
 
-                if (pageBlock && pageBlock.innerHTML.trim().length) {
-                    pageResults.innerHTML = pageBlock.innerHTML
-                    let btnAddWishlistAll =
-                        pageResults.querySelectorAll(".js-btn-wishlist")
-                    let pageResultsItem =
-                        pageResults.querySelectorAll(".js-card-item")
-
-                    pageResultsItem.forEach((item) => {
-                        let media = item.querySelector(".js-card-product-media")
-                        media.classList.add("media__size--" + imageSize)
-                        media.setAttribute("data-size", imageSize)
-                    })
-
-                    btnAddWishlistAll.forEach((item) => {
-                        item.addEventListener("click", (event) => {
-                            event.preventDefault()
-                            const element = event.currentTarget
-                            setTimeout(() => {
-                                element.closest(".js-card-item").remove()
-                            }, 300)
+                        btnAddWishlistAll.forEach((item) => {
+                            item.addEventListener("click", (event) => {
+                                event.preventDefault()
+                                const element = event.currentTarget
+                                setTimeout(() => {
+                                    element.closest(".js-card-item").remove()
+                                    let getRestProducts =
+                                        JSON.parse(
+                                            localStorage.getItem(
+                                                "productsWishlist"
+                                            )
+                                        ) || []
+                                    if (getRestProducts.length == 0) {
+                                        pageResults.classList.add("hidden")
+                                        titleResults.classList.remove("hidden")
+                                    }
+                                }, 300)
+                            })
                         })
-                    })
-                }
-            })
-            .catch((error) => {
-                console.error("Error:", error)
-            })
+                    }
+                })
+                .catch((error) => {
+                    console.error("Error:", error)
+                })
+        }
     }
 }
 
